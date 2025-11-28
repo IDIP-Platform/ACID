0dcad18029c17fa84d46082c25a564bba3313972
Alessandro Ulivi <alessandro.ulivi.89@gmail.com>
2025-11-28 10:19:21 +0100

    create format_str.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,2 @@
-__all__ = ["listdirNHF", "mksubdir", "open_image","save_image","label_image_utils", "get_defaults"]
+__all__ = ["listdirNHF", "mksubdir", "open_image","save_image","label_image_utils", "get_defaults",
+           "format_str"]
---
+++ utils/format_str.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+def format_series_str(input_series:pd.Series,
+                      symb_ols:list=['~', '!', '@', '#', '$', '%', '^', '&', '*',
+                                     '(', ')', '`', ';', '<', '>', '.', '?', ',', '[',
+                                     ']', '{', '}', '|', '°', '§', '/', 'ß', '+', '-'],
+                      spa_ces:list=[' ', '  ', '   '],
+                      symb_ol_replace:str='_',
+                      spa_ce_replace:str='')->pd.Series:
+    """
+    Formats a pandas series of strings by:
+    - substituting all symbols in symb_ols with symb_ol_replace (default '_')
+    - substituting all spaces in spa_ces with spa_ce_replace (default '', which leads dropping the spaces)
+    - making all caracters lowercase.
+
+    Returns the formatted series (as a copy).
+    """
+    # copy the input series
+    input_series_copy = input_series.copy()
+
+    # replace all symbols in symb_ols with what indicatd in symb_ol_replace
+    for symb_ol in symb_ols:
+    
+        input_series_copy = input_series_copy.str.replace(symb_ol,symb_ol_replace)
+    
+    # replace all spaces in spa_ces with what indicated in spa_ce_replace
+    for spa_ce in spa_ces:
+        
+        input_series_copy = input_series_copy.str.replace(spa_ce, spa_ce_replace)
+    
+    # tranform all characters to lowercase
+    input_series_copy.str.lower()
+
+    return input_series_copy

47b0562d531e6c19ad712fc3fd53c3aff5c25cbc
Alessandro Ulivi <ale.ulivi@gmail.com>
2025-12-05 11:00:29 +0100

    get default file from last modification date of a file in default_file_name

--- utils/get_defaults.py
+++ utils/get_defaults.py
@@ -2,6 +2,7 @@ import os
 import datetime
 
 def default_file_name(file_list:list,
+                      from_file_name:bool=False,
                       separator:str="_",
                       date_position:int=0,
                       date_format:str='%Y%m%d',
@@ -16,14 +17,27 @@ def default_file_name(file_list:list,
     # iterate through the target files
     for f in file_list:
 
-        # split file name and collect the date
-        f_date = datetime.datetime.strptime(f.split(separator)[date_position], date_format)
+        # get date from file name or...
+        if from_file_name:
+            # split file name and collect the date
+            f_date = datetime.datetime.strptime(f.split(separator)[date_position], date_format)
 
-        # collect date into dates
-        dates.append(f_date)
+            # collect date into dates
+            dates.append(f_date)
 
-        # link date to original file name 
-        file_date_link[f_date]=f
+            # link date to original file name 
+            file_date_link[f_date]=f
+        
+        # ...get date from last modification date of the file
+        else:
+            # get the last modification date of the file
+            f_date = datetime.datetime.fromtimestamp(os.path.getmtime(f))
+
+            # collect date into dates
+            dates.append(f_date)
+
+            # link date to original file name 
+            file_date_link[f_date]=f
     
     # sort dates
     sorted_dates = sorted(dates, reverse=reverse)

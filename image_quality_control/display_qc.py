be5cb52340ac9223714e301ce58bc7c0fb851fd4
Alessandro Ulivi <alessandro.ulivi.89@gmail.com>
2026-01-09 17:20:01 +0100

    visualiye qcs

--- image_quality_control/display_qc.py
+++ image_quality_control/display_qc.py
@@ -34,7 +34,7 @@ def high_low_QC_fov(metadata_df,
     return img_low, img_high, fov_row_low, fov_row_high
 
 
-def display_qc(metadata_df,
+def display_high_low_qc(metadata_df,
                napari_viewer,
                qc_clm_collection,
                fov_clm,
@@ -64,3 +64,28 @@ def display_qc(metadata_df,
     # print message if visualization fails
     except:
         print("could not visualize fields of view with min-max QCs in napari")
+
+
+def display_qc(df_to_display,
+               napari_viewer,
+               metadata_df,
+               fov_column_name,
+               fov_directory,
+               channel_to_display,
+               channel_axis,
+               qc_to_display,
+               ch_measurement_separator):
+
+    for row in df_to_display.index:
+
+        # get the fov name
+        fov_to_display_name= metadata_df.loc[row, fov_column_name]
+
+        # open the images
+        fov_to_display = tifffile.imread(os.path.join(fov_directory, str(fov_to_display_name)))
+
+        # select the channel to visualize
+        fov_ch_to_display = fov_to_display.take(indices=channel_to_display, axis=channel_axis)
+
+        # visualize using napari
+        napari_viewer.add_image(fov_ch_to_display, name=f"{qc_to_display}: {metadata_df.loc[row, f"{qc_to_display}{ch_measurement_separator}{channel_to_display}"]}, {fov_to_display_name}")

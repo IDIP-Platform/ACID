a3316ebbbe8741c77faf5b10f1d54a9c9323533f
Alessandro Ulivi <ale.ulivi@gmail.com>
2026-02-10 16:29:08 +0100

    implement map_layout_condition

--- data_preparation/map_category.py
+++ data_preparation/map_category.py
@@ -1,5 +1,7 @@
 import pandas as pd
-from utils.str_utils import extract_number
+from collections.abc import Sequence
+from utils.str_utils import extract_number, split_text_and_number
+
 
 def map_fov_categories(plate_layout_df:pd.DataFrame,
                        well:int,
@@ -64,3 +66,61 @@ def map_fov_categories_df(metadata_df:pd.DataFrame,
                                                                                            treatment_column=treatment__column), axis=1)
 
     return original_metadata_df
+
+def map_layout_condition(condition_list:Sequence,
+                         number_type:int|float|None=None,
+                         regex:str|None=None,
+                         null_value:tuple=(None, None),
+                         return_unique:bool=True)->tuple:
+    """
+    Splits a string into a text prefix and a trailing numeric part.
+
+    Parameters
+    ----------
+    s : str
+        Input string expected to end with one or more digits (no separator).
+    number_type : type or None, optional
+        If None, the numeric part is returned as a string.
+        If a type is provided (e.g. int, float), the numeric part is
+        converted using that type.
+
+    number_type: int, float or None. Optional. Default None.
+        If int or float, the function will try to convert the second part of the splat string
+        in, respectively an integer or a float.
+        If None (default), non type conversion will be tried.
+    
+    regex: regex str|None. Optional, default r'^(.*?)(\d+)$'
+        The regex expression to use for splitting the input string.
+    
+    null_value. tuple. Optional, default (None, None).
+        The value returned if the input string can't be split using the input regex expression.
+
+    
+    Returns
+    -------
+    tuple
+        (prefix, number) if the string ends with digits.
+        If no trailing number is found, (None, None) is returned.
+    """
+    if regex is None:
+        regex = r'^(.*?)(\d+)$'
+    
+    prefixes = []
+    suffixes = []
+    for c in condition_list:
+        pre_fix, suf_fix = split_text_and_number(c,
+                                                 regex=regex,
+                                                 number_type=number_type,
+                                                 null_value=null_value)
+        prefixes.append(pre_fix)
+        suffixes.append(suf_fix)
+
+    if return_unique:
+        unique_prefixes = list(set(prefixes))
+        unique_suffixes = list(set(suffixes))
+        return unique_prefixes, unique_suffixes
+    else:
+        return prefixes, suffixes
+
+
+
